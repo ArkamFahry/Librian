6caa7ac0c163d1a70f1637e28d3084fcc1155d9b
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-03-08 21:40:23 +0530

    Implemented part of the main window

--- app/trackx_desktop/App.axaml.cs
+++ app/trackx_desktop/App.axaml.cs
@@ -22,7 +22,7 @@ namespace trackx_desktop
                 // Line below is needed to remove Avalonia data validation.
                 // Without this line you will get duplicate validations from both Avalonia and CT
                 ExpressionObserver.DataValidators.RemoveAll(x => x is DataAnnotationsValidationPlugin);
-                desktop.MainWindow = new MainWindow
+                desktop.MainWindow = new MainWindow()
                 {
                     DataContext = new MainWindowViewModel(),
                 };
